d807b7b12779b20ab93123c0458464654e586511
Hakan Vardar <hakovardo123@gmail.com>
2023-12-23 20:13:30 +0300

    AAAAAAAAAAAAAAAAAAAA

--- src/server.rs
+++ src/server.rs
@@ -42,9 +42,7 @@ impl<'a> Server<'a> {
             App::new()
                 .wrap(
                     Cors::default()
-                        .allowed_origin("https://todoapph.netlify.app/register")
-                        .allowed_origin("https://todoapph.netlify.app/login")
-                        .allowed_origin("https://todoapph.netlify.app")
+                        .allow_any_origin()
                         .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
                         .expose_any_header()
                         .max_age(3600),
