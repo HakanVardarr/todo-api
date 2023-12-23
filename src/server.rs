4716e9558c591e71b69313f84ce9c6f2b7f4dc33
Hakan Vardar <hakovardo123@gmail.com>
2023-12-23 10:12:33 +0300

    test

--- src/server.rs
+++ src/server.rs
@@ -40,39 +40,6 @@ impl<'a> Server<'a> {
 
         HttpServer::new(move || {
             App::new()
-                .wrap(
-                    // default settings are overly restrictive to reduce chance of
-                    // misconfiguration leading to security concerns
-                    Cors::default()
-                        // add specific origin to allowed origin list
-                        .allowed_origin("http://localhost:8080")
-                        // allow any port on localhost
-                        .allowed_origin_fn(|origin, _req_head| {
-                            origin.as_bytes().starts_with(b"http://localhost")
-
-                            // manual alternative:
-                            // unwrapping is acceptable on the origin header since this function is
-                            // only called when it exists
-                            // req_head
-                            //     .headers()
-                            //     .get(header::ORIGIN)
-                            //     .unwrap()
-                            //     .as_bytes()
-                            //     .starts_with(b"http://localhost")
-                        })
-                        // set allowed methods list
-                        .allowed_methods(vec!["GET", "POST"])
-                        // set allowed request header list
-                        .allowed_headers(&[header::AUTHORIZATION, header::ACCEPT])
-                        // add header to allowed list
-                        .allowed_header(header::CONTENT_TYPE)
-                        // set list of headers that are safe to expose
-                        .expose_headers(&[header::CONTENT_DISPOSITION])
-                        // allow cURL/HTTPie from working without providing Origin headers
-                        .block_on_origin_mismatch(false)
-                        // set preflight cache TTL
-                        .max_age(3600),
-                )
                 .wrap(Logger::new(r#"%a "%r" %s %T"#))
                 .app_data(web::Data::new(client.clone()))
                 .service(services![
